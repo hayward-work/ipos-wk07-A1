d314160f9c3929e55a2f1871b95c7ed0dbc8c1fb
20125011 <20125011@tafe.wa.edu.au>
2025-09-10 15:06:12 +0800

    test: add test for subtract function

--- test_functions.py
+++ test_functions.py
@@ -1,13 +1,14 @@
-from functions import add
-
+from functions import add, subtract, multiply
+from functions import convert_fahrenheit_to_celsius as f2c
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+   assert subtract(2, 3) == -1
 
 # uncomment the following test in step 11
 # def test_convert_fahrenheit_to_celsius():
